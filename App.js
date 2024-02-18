e8d5d9708f9611f2e559202529093e037d007a29
lindster88 <30700148+lindster88@users.noreply.github.com>
2024-02-18 04:11:15 -0800

    Frontend done!
    needs nav though.

--- App.js
+++ App.js
@@ -68,10 +68,24 @@ export default function App() {
     // button time!! gives you all the buttons
     return ( // what's actually on display
       <SafeAreaView style={styles.container}> 
-        <Image style={styles.preview} source={{ uri: "data:image/jpg;base64," + photo.base64 }} /> 
-        <Button title="Share" onPress={sharePic} /> 
-        {hasMediaLibraryPermission ? <Button title="Save" onPress={savePhoto} /> : undefined}
-        <Button title="Discard" onPress={() => setPhoto(undefined)} />
+      <ImageBackground style={styles.background} source={require("./assets/camerabg.png")}>
+        <Image style={styles.camera} source={{ uri: "data:image/jpg;base64," + photo.base64 }} /> 
+        {/* <Button title="Share" onPress={sharePic} />  */}
+        {/* {hasMediaLibraryPermission ? <Button title="Save" onPress={savePhoto} /> : undefined} */}
+        {/* <Button title="Discard" onPress={() => setPhoto(undefined)} /> */}
+        <TouchableOpacity style={styles.button2} onPress={savePhoto}>
+          <Text style={styles.buttonText2}>Save Picture</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button3} onPress={savePhoto}>
+          <Text style={styles.buttonText3}>Discard</Text>
+        </TouchableOpacity>
+        <Text style={styles.titleText}>
+          Scanned!
+        </Text>
+        <Text style={styles.titleSubtext}>
+          that's a QR code.
+        </Text>
+      </ImageBackground>
       </SafeAreaView>
     );
     // image style...: displays image that you just took as a jpeg
@@ -131,7 +145,7 @@ const styles = StyleSheet.create({
   titleSubtext:{
     position: "absolute",
     color: "#110C48",
-    fontSize: 50,
+    fontSize: 45,
     fontFamily: 'League Spartan Regular',
     top: "71%",
     marginLeft: "10%"
@@ -157,6 +171,42 @@ const styles = StyleSheet.create({
     fontFamily: 'Itim',
     marginTop: "17%",
     marginLeft: "12%"
+  },
+  buttonText2:{
+    color: "#110C48",
+    fontSize: 35,
+    fontFamily: 'Itim',
+    marginTop: "5%",
+    marginLeft: "13%",
+    width: "75%",
+    textAlign:"center"
+  },
+  button2:{
+    position: "absolute",
+    top: "82%",
+    left: "50%",
+    right: 30,
+    bottom: 40,
+    borderRadius: 25,
+    backgroundColor: "#9290B4" // light purple
+  },
+  buttonText3:{
+    color: "#110C48",
+    fontSize: 40,
+    fontFamily: 'Itim',
+    marginTop: "17%",
+    marginLeft: "10%",
+    width: "80%",
+    textAlign:"center"
+  },
+  button3:{
+    position: "absolute",
+    top: "82%",
+    left: "7%",
+    right: "53%",
+    bottom: 40,
+    borderRadius: 25,
+    backgroundColor: "#9290B4" // light purple
   },
   buttonContainer: {
     backgroundColor: '#fff', // white
